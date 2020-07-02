4c8e3da011f3a35710f60d1432c1f93936814d1f
Ritik Mittal <ritikmittal13@gmail.com>
2020-07-02 13:55:57 +0530

    Backend setup complete

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.json({ msg: "Hello World" }));
 
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
